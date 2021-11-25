4a2e58a0521d29e36765a2debf762f927f6bd06f
Rod <48632799+RodWolt@users.noreply.github.com>
2021-11-25 16:52:51 -0300

    Update cadastroMetaProjetoMaker.c

--- cadastroMetaProjetoMaker.c
+++ cadastroMetaProjetoMaker.c
@@ -24,92 +24,136 @@ float cadastroMeta()
 		int mes, check, i=1;
 		//Escolher quantos meses;
 		printf("Gostaria de cadastrar o consumo de -3-, -6- ou -12- meses?\n>: ");
+		fflush(stdin);
 		scanf("%d", &mes);
 		switch (mes)
 		{
 			case 3:
 				do{
-					printf("-------------\nConsumo do mes %d\n>:", i);
+					system("cls");
+					printf("-------------\nConsumo do mês %d\n>:", i);
+					fflush(stdin);
 					scanf("%f",&cons);
 					consumo+=cons;
 					i++;
 				}while(i<=mes);
 				//Deixei as 2 formas de adicionar a meta, de acordo com a discussão que tivemos, a meta vai ser salva em % do valor original
-				printf("-------------\nComo gostaria de estipular sua meta? -1- para usar Porcentagem, -2- para usar Numero inteiro\n>: ");
+				system("cls");
+				printf("-------------\nConsumo total nos %d meses>: %.2f kwatts", mes, consumo);
+				printf("\n-------------\nComo gostaria de estipular sua meta? -1- para usar Porcentagem, -2- para usar Numero inteiro\n>: ");
+				fflush(stdin);
 				scanf("%d", &check);
 				if(check==1)
 				{
+					system("cls");
 					printf("-------------\nQuanto gostaria de economizar?(Porcentagem)\n>: ");
+					fflush(stdin);
 					scanf("%f", &meta);
 					meta2 = consumo - (consumo * (meta/100));//inteiro atraves da %
 					metaFinal = meta;
-					printf("-------------\nPara economizar %.2f%%, voce devera consumir %.2f kwatts em %d meses.", meta, meta2, mes);
+					system("cls");
+					printf("-------------\nPara economizar %.2f%%, você deverá consumir %.2f kwatts em %d meses.", meta, meta2, mes);
 					break;
 				}
 				else if(check==2)
 				{
+					system("cls");
 					printf("-------------\nQuanto gostaria de economizar?(Inteiro)\n>: ");
+					fflush(stdin);
 					scanf("%f", &meta);
-					meta2 = (meta/consumo)*100; //porcentagem
-					metaFinal = meta2;
-					printf("-------------\nQuer economizar %.2f%% de seu consumo anterior em %d meses.", meta2, mes);
+					metaFinal = (meta/consumo)*100; // porcentagem
+					meta2 = consumo - meta;
+					system("cls");
+					printf("-------------\nQuer economizar %.2f%% de seu consumo anterior em %d meses.", metaFinal, mes);
+					printf("\n-------------\nPara economizar %.2f%%, você deverá consumir %.2f kwatts em %d meses.", metaFinal, meta2, mes);
 					break;
 				}
 			case 6:
 				do{
-					printf("-------------\nConsumo do mes %d\n>:", i);
+					system("cls");
+					printf("-------------\nConsumo do mês %d\n>:", i);
+					fflush(stdin);
 					scanf("%f",&cons);
 					consumo+=cons;
 					i++;
 				}while(i<=mes);
-				printf("-------------\nComo gostaria de estipular sua meta? -1- para usar Porcentagem, -2- para usar Numero inteiro\n>: ");
+				system("cls");
+				printf("-------------\nConsumo total nos %d meses>: %.2f kwatts", mes, consumo);
+				printf("\n-------------\nComo gostaria de estipular sua meta? -1- para usar Porcentagem, -2- para usar Numero inteiro\n>: ");
+				fflush(stdin);
 				scanf("%d", &check);
 				if(check==1)
 				{
+					system("cls");
 					printf("-------------\nQuanto gostaria de economizar?(Porcentagem)\n>: ");
+					fflush(stdin);
 					scanf("%f", &meta);
 					meta2 = consumo - (consumo * (meta/100));
 					metaFinal = meta;
-					printf("-------------\nPara economizar %.2f%%, voce devera consumir %.2f kwatts em %d meses.", meta, meta2, mes);
+					system("cls");
+					printf("-------------\nPara economizar %.2f%%, você deverá consumir %.2f kwatts em %d meses.", meta, meta2, mes);
 					break;
 				}
 				else if(check==2)
 				{
+					system("cls");
 					printf("-------------\nQuanto gostaria de economizar?(Inteiro)\n>: ");
+					fflush(stdin);
 					scanf("%f", &meta);
-					meta2 = (meta/consumo)*100;
-					metaFinal = meta2;
-					printf("-------------\nQuer economizar %.2f%% de seu consumo anterior em %d meses.", meta2, mes);
+					metaFinal = (meta/consumo)*100;
+					meta2 = consumo - meta;
+					system("cls");
+					printf("-------------\nQuer economizar %.2f%% de seu consumo anterior em %d meses.", metaFinal, mes);
+					printf("\n-------------\nPara economizar %.2f%%, você deverá consumir %.2f kwatts em %d meses.", metaFinal, meta2, mes);
 					break;
 				}
 			case 12:
 				do{
-					printf("-------------\nConsumo do mes %d\n>:", i);
+					system("cls");
+					printf("-------------\nConsumo do mês %d\n>:", i);
+					fflush(stdin);
 					scanf("%f",&cons);
 					consumo+=cons;
 					i++;
 				}while(i<=mes);
-				printf("-------------\nComo gostaria de estipular sua meta? -1- para usar Porcentagem, -2- para usar Numero inteiro\n>: ");
+				system("cls");
+				printf("-------------\nConsumo total nos %d meses>: %.2f kwatts", mes, consumo);
+				printf("\n-------------\nComo gostaria de estipular sua meta? -1- para usar Porcentagem, -2- para usar Numero inteiro\n>: ");
+				fflush(stdin);
 				scanf("%d", &check);
 				if(check==1)
 				{
+					system("cls");
 					printf("-------------\nQuanto gostaria de economizar?(Porcentagem)\n>: ");
+					fflush(stdin);
 					scanf("%f", &meta);
 					meta2 = consumo - (consumo * (meta/100));
 					metaFinal = meta;
-					printf("-------------\nPara economizar %.2f%%, voce devera consumir %.2f kwatts em %d meses.", meta, meta2, mes);
+					system("cls");
+					printf("-------------\nPara economizar %.2f%%, você deverá consumir %.2f kwatts em %d meses.", meta, meta2, mes);
 					break;
 				}
 				else if(check==2)
 				{
+					system("cls");
 					printf("-------------\nQuanto gostaria de economizar?(Inteiro)\n>: ");
+					fflush(stdin);
 					scanf("%f", &meta);
-					meta2 = (meta/consumo)*100;
-					metaFinal = meta2;
-					printf("-------------\nQuer economizar %.2f%% de seu consumo anterior em %d meses.", meta2, mes);
+					metaFinal = (meta/consumo)*100;
+					meta2 = consumo - meta;
+					system("cls");
+					printf("-------------\nQuer economizar %.2f%% de seu consumo anterior em %d meses.", metaFinal, mes);
+					printf("\n-------------\nPara economizar %.2f%%, você deverá consumir %.2f kwatts em %d meses.", metaFinal, meta2, mes);
 					break;
 				}
+			default:
+               	printf("-------------\nDigite uma opção valida.\n-------------\n");
+               	cadastroMeta();
+               	break;
 				
+		}
+		printf("\nConsumo inicial>: %.2f kwatts", consumo);
+		printf("\nMeta Final>: %.2f%%", metaFinal);
 		return consumo;
 		return metaFinal;			
 		}
